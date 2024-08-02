b8ee5b1be91b0d1bf0720db8e278aba57c4f516e
AndreaMargutti <marguttiandrea30@gmail.com>
2024-08-02 17:54:07 +0200

    esercizio finito

--- js/script.js
+++ js/script.js
@@ -15,33 +15,40 @@ let number;
 //Recupero elemento nel DOM
 const numberTable = document.getElementById('number-table');
 console.log(numberTable);
+
 //Creazione variabile per lista
-let numberList = '<ul>';
+const list = document.createElement('ul');
 
 // # Fase di elaborazione dati
 //Generiamo i numeri da 1 a 100
 for(let i = 1; i < 101; i++){
     number = i;
+    const listItem = document.createElement('li');
     
-    //Assegnamo le stringe ai multipli
+    //Assegnamo multipli e colori di sfondo
     if (i % 5 == 0 && i % 3 == 0){
         number = 'FizzBuzz';
+        listItem.classList.add('bg-red');
         console.log(number);
     } else if (i % 3 == 0) {
         number = 'Fizz';
+        listItem.classList.add('bg-blue');
         console.log(number);
     } else if (i % 5 == 0) {
         number = 'Buzz';
+        listItem.classList.add('bg-yellow');
         console.log(number);
     } else {
+        listItem.classList.add('bg-gray');
         console.log(i);
     }
-    //Inseriemento numero nella lista
-    numberList += `<li>${number}</li>`;
+
+    //Inseriemento li nella lista
+    listItem.append(number);
+    list.appendChild(listItem);
+    listItem.classList.add('fw-bold');
 }
 
-//Chiusura Lista
-numberList += '</ul>';
-numberTable.innerHTML = numberList;
-
-// # Stile
+// # Fase di Output
+//Inserimento Lista in pagina
+numberTable.appendChild(list);
