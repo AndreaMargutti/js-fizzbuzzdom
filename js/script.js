cd7216c74663525072fddd0ff107260381b1e72b
AndreaMargutti <marguttiandrea30@gmail.com>
2024-08-02 15:46:13 +0200

    console log for 100

--- js/script.js
+++ js/script.js
@@ -1,1 +1,16 @@
-console.log('JS OK');
+/* Scaletta Mentale
+1. Generare i numeri da 1 a 100
+2. Stampare i numeri in console
+3. Assegnare le parole ai multipili
+    3.1. Multipili di 3 = Fizz
+    3.2. Multipli di 5 = Buzz
+    3.3. Multipli di 3 e 5 = FizzBuzz
+4. Stamparli in pagina
+*/
+
+// # Fase di elaborazione dati
+//Generiamo i numeri da 1 a 100
+for(let i = 1; i < 101; i++){
+    console.log(i);
+
+}
